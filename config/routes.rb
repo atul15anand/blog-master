cb7271158e1027a8e3980db0a51a146bb2df5e13
alma <atul15anand@gmail.com>
2020-01-16 11:31:17 +0530

    users creation

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
 	root 'welcome#index'
 	get 'welcome/about', to: 'welcome#about'
-
+	get 'signup', to: 'users#new'
 	resources  :articles
+	resources :users, except: [:new]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
