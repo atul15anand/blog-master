6f874f546046f1eb2b237838bf04f1483adcd7be
alma <atul15anand@gmail.com>
2020-01-19 19:32:02 +0530

    admin user added to all

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:edit,:update,:show]
+  before_action :require_same_user,only: [:edit,:update,:destroy]
+  before_action :require_admin,only: [:destroy]
   
   def index
     @users = User.paginate(page: params[:page], per_page: 3)
@@ -37,6 +39,13 @@ class UsersController < ApplicationController
     @user_articles = @user.articles.paginate(page: params[:page],per_page: 4)
   end
 
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    flash[:danger] = "User and all its artices have been destroyed."
+    redirect_to users_path
+  end
+
   private
   def user_params
     params.require(:user).permit(:username,:email,:password)
@@ -44,5 +53,19 @@ class UsersController < ApplicationController
 
   def set_user
     @user = User.find(params[:id])
+  end
+
+  def require_same_user
+    if current_user!= @user && !current_user.admin?
+      flash[:danger] = "You can only edit your own account."
+      redirect_to edit_user_path(@user)
+    end
+  end
+
+  def require_admin
+    if logged_in? and !current_user.admin?
+      flash[:danger] = "Only admin can do that"
+      redirect_to root_path
+    end
   end
 end
